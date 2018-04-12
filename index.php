14a0cf2e7de8e81f075a437f1e56063b99eb49b0
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-04-11 23:46:45 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php # require('FILE.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Clayton Phillips-Dorsett Homepage</title>
+    <meta charset="utf-8">
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- AOS css -->
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+
+    <!-- Stylesheet Links -->
+    <link rel="stylesheet" href="css/cpd_styles.css">
+
+</head>
+<body>
+
+
+<!-- landing page -->
+<?php include("landing.php"); ?>
+
+
+<!-- JQuery js -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+
+<!-- Font Awesome js -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js"
+        integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+"
+        crossorigin="anonymous"></script>
+
+<!-- AOS js -->
+<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+
+<!-- Bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Other script js -->
+<script src="js/cpd_js.js"></script>
+
+</body>
+</html>
---
+++ landing.php
@@ -0,0 +1,46 @@
+<?php ?>
+
+<header id="headerDiv" class="d-flex justify-content-center align-items-center">
+    <div>
+        <div id="headerCpd" class="d-block p-3 font-weight-bold" data-aos="fade-down" data-aos-duration="1200">
+            <span>Clayton Phillips-Dorsett</span>
+        </div>
+        <div id="headerText" class="d-block text-center">
+            <div data-aos="zoom-out-right" data-aos-delay="1000" data-aos-duration="1500">
+                <span class="d-block p-1"><i class="fas fa-chart-bar"></i> Visualization</span>
+            </div>
+            <div data-aos="zoom-out-left" data-aos-delay="1200" data-aos-duration="1500">
+                <span class="d-block p-1"><i class="fab fa-connectdevelop"></i> Web Development</span>
+            </div>
+            <div data-aos="zoom-out-right" data-aos-delay="1400" data-aos-duration="1500">
+                <span class="d-block p-1"><i class="far fa-file-code"></i> Data Programming</span>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<div id="visDiv" class="col-md-8 offset-md-2">
+    Extensive experience providing insight to stakeholders through technical visualizations.
+    <ul>
+        <li>Advanced familiarity with R's premier ggplot2 library</li>
+        <li>Applied experience with highcharts.js (shown above), plotly.js, and Python's Matplotlib</li>
+    </ul>
+</div>
+
+
+<div id="webDevDiv" class="col-md-8 offset-2">
+    My current focus and area of greatest interest.  Developing models and analyzing data within a standalone script
+    is one thing, but bringing them to life interactively on the web to share with a broad audience holds the greatest
+    value.
+    <ul>
+        <li>Application frameworks built with Laravel (see "StockTracker" above) and Django ("QuickModel")</li>
+        <li>Experience with production environments and ORM interaction with databases</li>
+        <li>Proficient with JQuery and Bootstrap for front-end development</li>
+    </ul>
+</div>
+
+
+<div id="dataDiv">
+
+</div>
