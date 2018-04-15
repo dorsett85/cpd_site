8575c784bfaa5fe93563fdb16cb3b8a520a68b88
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-04-15 19:00:38 -0400

    Changed stylesheet and script order

--- index.php
+++ index.php
@@ -6,12 +6,11 @@
     <title>Clayton Phillips-Dorsett Homepage</title>
     <meta charset="utf-8">
 
-    <!-- Bootstrap css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
     <!-- AOS css -->
     <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
 
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!-- Stylesheet Links -->
     <link rel="stylesheet" href="css/cpd_styles.css">
@@ -23,6 +22,8 @@
 <!-- landing page -->
 <?php include("landing.php"); ?>
 
+<!-- AOS js -->
+<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
 
 <!-- JQuery js -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
@@ -36,9 +37,6 @@
 <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js"
         integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+"
         crossorigin="anonymous"></script>
-
-<!-- AOS js -->
-<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
 
 <!-- Highcharts js -->
 <script src="https://code.highcharts.com/stock/highstock.js"></script>
